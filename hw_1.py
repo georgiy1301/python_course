28c428ece63f267151ff5f1e58082e4cf34075ac
georgiy1301 <79781242+georgiy1301@users.noreply.github.com>
2021-05-06 11:05:50 +0300

    Delete hw_1.py

--- hw_1.py
+++
@@ -1,8 +0,0 @@
-a = ['Georgiy', 1.25, 'Hello world', True, 18, False]
-print(type(a))
-print(type(a[0]))
-print(type(a[1]))
-print(type(a[2]))
-print(type(a[3]))
-print(type(a[4]))
-print(type(a[5]))
