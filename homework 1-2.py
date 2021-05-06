c51fdb2d176587492158351d71240034166e121a
georgiy1301 <g130184@yandex.ru>
2021-05-06 12:37:12 +0300

    Задание 2

---
+++ homework 1-2.py
@@ -0,0 +1,6 @@
+n = input("Введите положительное число - ")
+num1 = n
+num2 = n + n
+num3 = n + n + n
+sum = int(num1) + int(num2) + int(num3)
+print(f'Получиличь следующие значения: {n} + {n + n} + {n + n + n}. Их сумма равна {sum}')
