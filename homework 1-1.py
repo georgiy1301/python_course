7dfc23765961d75e872b8b45059fd5338c87a8a1
georgiy1301 <79781242+georgiy1301@users.noreply.github.com>
2021-05-11 22:28:28 +0300

    Delete homework 1-1.py

--- homework 1-1.py
+++
@@ -1,7 +0,0 @@
-user_time = int(input('Введите ваше время в секундах: '))
-hour = user_time // 3600
-min = int((user_time % 3600)/60)
-sec = user_time % 60
-
-time = f'{hour:02}:{min:02}:{sec:02}'
-print(time)
